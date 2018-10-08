142800f1a6dfc0da73ea7960ab37cda4a6abf6c9
Alam García Roldán <garcia76@avaya.com>
2018-10-08 16:32:48 -0500

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+console.log("Carga De DOM");
+//Variables globales
+//Valores por defecto
+var endpoint = "http://breeze2-213.collaboratory.avaya.com/services/EventingConnector/events";
+var family = "AAAMIAETIQUETAS";
+var version = "1.0";
+//Fin de Variables globales+
+
+//Listeners botones
+//Listener Boton Ajustes
+$("#ajustes-btn" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+
+
+});
