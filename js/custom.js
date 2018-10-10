1bce3ee16aa22efee2b1db8382c821d9ef87ef2b
Alam García Roldán <garcia76@avaya.com>
2018-10-10 11:23:18 -0500

    Se agrega Validacion a Descricpción de Prooducto Ajustes en el código de Post

--- js/custom.js
+++ js/custom.js
@@ -51,106 +51,146 @@ $(document).ready(function() {
   //Listeners para formularios
   ///Ajustes
   $("#ajustes-frm").submit(function() {
-   event.preventDefault();
-   var datos = $("#ajustes-frm").serializeArray();
-   var formData = JSON.parse(JSON.stringify(jQuery('#ajustes-frm').serializeArray()))
-   console.log(datos);
-   var endpoint = datos["0"].value;
-   // var debugmode = datos["1"].value;
-   localData.set('endpoint_v', endpoint);
-   // localData.set('debug_v', debugmode);
+    event.preventDefault();
+    var datos = $("#ajustes-frm").serializeArray();
+    var formData = JSON.parse(JSON.stringify(jQuery('#ajustes-frm').serializeArray()))
+    console.log(datos);
+    var endpoint = datos["0"].value;
+    // var debugmode = datos["1"].value;
+    localData.set('endpoint_v', endpoint);
+    // localData.set('debug_v', debugmode);
 
-   $('#modal-ajustes').modal('hide');
-   $('#modal-informacion').modal({
-                       backdrop: 'static',
-                       keyboard: true,
-                       show: true
-               });
-   $("#mensaje-modal").text("Tus ajustes se han guardado. Recargando");
-   setTimeout(function() {
-     location.reload(true);
-   }, 5000);
- });
- ///Precio
- $("#precio-frm").submit(function() {
-  btype = "AAAMIAETIQUETASPRECIO"
-  event.preventDefault();
-  var preciofrm = $("#precio-frm").serializeArray();
+    $('#modal-ajustes').modal('hide');
+    $('#modal-informacion').modal({
+      backdrop: 'static',
+      keyboard: true,
+      show: true
+    });
+    $("#mensaje-modal").text("Tus ajustes se han guardado. Recargando");
+    setTimeout(function() {
+      location.reload(true);
+    }, 5000);
+  });
+  ///Precio
+  $("#precio-frm").submit(function() {
+    btype = "AAAMIAETIQUETASPRECIO"
+    event.preventDefault();
+    var preciofrm = $("#precio-frm").serializeArray();
     console.log(preciofrm);
-  var emailval = preciofrm["0"].value;
-  var smsval = preciofrm["1"].value;
-  var prodval = preciofrm["2"].value;
-  var obj = "{\"email\":\""+ emailval +"\",\"sms\":\""+ smsval +"\",\"codigoProducto\":\""+prodval+"\"}";
+    var emailval = preciofrm["0"].value;
+    var smsval = preciofrm["1"].value;
+    var prodval = preciofrm["2"].value;
+    var obj = "{\"email\":\"" + emailval + "\",\"sms\":\"" + smsval + "\",\"codigoProducto\":\"" + prodval + "\"}";
 
 
-  postbreeze(family, btype, version, endpoint, obj)
-});
-///Precio
-$("#descr-frm").submit(function() {
-  btype1 = "AAAMIAETIQUETASDESCRIPCION";
- event.preventDefault();
- var descrfrm = $("#descr-frm").serializeArray();
- var phoneval1 = descrfrm["0"].value;
- var prodval1 = descrfrm["1"].value;
- var obj1 = "{\"mobileNumber\":\""+ phoneval1 +"\",\"productcode\":\""+ prodval1 +"\"}";
- console.log(obj);
- postbreeze(family, btype1, version, endpoint, obj1)
-});
+    postbreeze(family, btype, version, endpoint, obj)
+  });
+  ///Precio
+  $("#descr-frm").submit(function() {
+    btype1 = "AAAMIAETIQUETASDESCRIPCION";
+    event.preventDefault();
+    var descrfrm = $("#descr-frm").serializeArray();
+    var phoneval1 = descrfrm["0"].value;
+    var prodval1 = descrfrm["1"].value;
+    var obj1 = "{\"mobileNumber\":\"" + phoneval1 + "\",\"productcode\":\"" + prodval1 + "\"}";
+    console.log(descrfrm);
+    postbreeze(family, btype1, version, endpoint, obj1)
+  });
 
-//Validacion E164
-var input = document.querySelector("#inputsms-txt"),
-  errorMsg = document.querySelector("#error-msg"),
-  validMsg = document.querySelector("#valid-msg");
+  //Validacion E164
+  var input = document.querySelector("#inputsms-txt"),
+    errorMsg = document.querySelector("#error-msg"),
+    validMsg = document.querySelector("#valid-msg");
+    var input2 = document.querySelector("#inputsms2-txt"),
+      errorMsg2 = document.querySelector("#error-msg2"),
+      validMsg2 = document.querySelector("#valid-msg2");
 
-// here, the index maps to the error code returned from getValidationError - see readme
-var errorMap = [ "Número no válido", "Código de País Incorrecto", "Muy Corto", "Muy largo", "Número Inválido"];
+  // here, the index maps to the error code returned from getValidationError - see readme
+  var errorMap = ["Número no válido", "Código de País Incorrecto", "Muy Corto", "Muy largo", "Número Inválido"];
 
-// initialise plugin
-var iti = window.intlTelInput(input, {
-  initialCountry: "auto",
-   geoIpLookup: function(callback) {
-     $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
-       var countryCode = (resp && resp.country) ? resp.country : "";
-       callback(countryCode);
-     });
-   },
-  nationalMode: true,
-  utilsScript: "js/utils.js"
-});
+  // initialise plugin
+  var iti = window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: function(callback) {
+      $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+        var countryCode = (resp && resp.country) ? resp.country : "";
+        callback(countryCode);
+      });
+    },
+    nationalMode: true,
+    utilsScript: "js/utils.js"
+  });
 
-var reset = function() {
-  input.classList.remove("error");
-  errorMsg.innerHTML = "";
-  errorMsg.classList.add("hide");
-  validMsg.classList.add("hide");
-};
+  var iti2 = window.intlTelInput(input2, {
+    initialCountry: "auto",
+    geoIpLookup: function(callback) {
+      $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+        var countryCode = (resp && resp.country) ? resp.country : "";
+        callback(countryCode);
+      });
+    },
+    nationalMode: true,
+    utilsScript: "js/utils.js"
+  });
 
-// on blur: validate
-input.addEventListener('blur', function() {
-  reset();
-  if (input.value.trim()) {
-    if (iti.isValidNumber()) {
-      validMsg.classList.remove("hide");
-      console.log(iti.getNumber());
-      $("input#intnumber-txt").val(iti.getNumber());
-      $("#submit-frm1").removeAttr('disabled');
-    } else {
-      input.classList.add("error");
-      var errorCode = iti.getValidationError();
-      errorMsg.innerHTML = errorMap[errorCode];
-      errorMsg.classList.remove("hide");
-      $("input#intnumber-txt").val('');
-      $("#elementID").prop("disabled", true);
+  var reset = function() {
+    input.classList.remove("error");
+    input2.classList.remove("error");
+    errorMsg.innerHTML = "";
+    errorMsg.classList.add("hide");
+    validMsg.classList.add("hide");
 
+    errorMsg2.innerHTML = "";
+    errorMsg2.classList.add("hide");
+    validMsg2.classList.add("hide");
+  };
+
+  // on blur: validate
+  input.addEventListener('blur', function() {
+    reset();
+    if (input.value.trim()) {
+      if (iti.isValidNumber()) {
+        validMsg.classList.remove("hide");
+        console.log(iti.getNumber());
+        $("input#intnumber-txt").val(iti.getNumber());
+        $("#submit-frm1").removeAttr('disabled');
+      } else {
+        input.classList.add("error");
+        var errorCode = iti.getValidationError();
+        errorMsg.innerHTML = errorMap[errorCode];
+        errorMsg.classList.remove("hide");
+        $("input#intnumber-txt").val('');
+        $("input#intnumber-txt").prop("disabled", true);
+
+      }
     }
-  }
-});
+  });
 
-// on keyup / change flag: reset
-input.addEventListener('change', reset);
-input.addEventListener('keyup', reset);
+  input2.addEventListener('blur', function() {
+    reset();
+    if (input2.value.trim()) {
+      if (iti2.isValidNumber()) {
+        validMsg2.classList.remove("hide");
+        console.log(iti2.getNumber());
+        $("input#intnumber2-txt").val(iti2.getNumber());
+        $("#submit-frm2").removeAttr('disabled');
+      } else {
+        input2.classList.add("error");
+        var errorCode = iti2.getValidationError();
+        errorMsg2.innerHTML = errorMap[errorCode];
+        errorMsg2.classList.remove("hide");
+        $("input#intnumber-txt2").val('');
+        $("input#intnumber-txt2").prop("disabled", true);
 
-//FIN Validacion
+      }
+    }
+  });
+
+  // on keyup / change flag: reset
+  input.addEventListener('change', reset);
+  input.addEventListener('keyup', reset);
+
+  //FIN Validacion
 
 
   //Fin Listeners
@@ -161,34 +201,34 @@ input.addEventListener('keyup', reset);
 //PostBreeze
 //Funciones
 function postbreeze(bfamily, btype, bversion, endpoint, eventBody) {
-	var data = new FormData();
-	data.append("family", bfamily);
-	data.append("type", btype);
-	data.append("version", bversion);
-	data.append("eventBody", eventBody);
-	try {
-		var postdata = $.ajax({
-			url: endpoint,
-			type: "POST",
-			data: data,
-			contentType: false,
-			cache: false,
-			processData: false,
-			error: function (xhr, status, errorThrown) {
-				console.log("Ha ocurrido un error: ");
-				console.log(xhr.statusText + " " + xhr.status);
-				alert(" Ha ocurrido un error ! ");
-			},
-			success: function (xhr, status, error, exception, event, options) {
-				console.log("Peticion Correcta");
-				console.log(status);
-				console.log(xhr.statusText);
-				alert(" Done ! ");
-			}
-		});
-	} catch (err) {
-		console.log(err.message);
-	}
+  var data = new FormData();
+  data.append("family", bfamily);
+  data.append("type", btype);
+  data.append("version", bversion);
+  data.append("eventBody", eventBody);
+  try {
+    var postdata = $.ajax({
+      url: endpoint,
+      type: "POST",
+      data: data,
+      contentType: false,
+      cache: false,
+      processData: false,
+      error: function(xhr, status, errorThrown) {
+        console.log("Ha ocurrido un error: ");
+        console.log(xhr.statusText + " " + xhr.status);
+        alert(" Ha ocurrido un error ! ");
+      },
+      success: function(xhr, status, error, exception, event, options) {
+        console.log("Peticion Correcta");
+        console.log(status);
+        console.log(xhr.statusText);
+        alert(" Done ! ");
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 //Fin Post
 //fin Funciones
