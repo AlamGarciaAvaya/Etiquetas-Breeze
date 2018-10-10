a5ca2a21e96009673af650dc9b67cc5b24891b2d
Alam García Roldán <garcia76@avaya.com>
2018-10-10 13:10:04 -0500

    Se cambia Endpoint a HTTPS

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+
+
+
   //Variables globales
   //Valores por defecto
-  var endpoint = "http://breeze2-213.collaboratory.avaya.com/services/EventingConnector/events";
+  var endpoint = "https://breeze2-213.collaboratory.avaya.com/services/EventingConnector/events";
   var family = "AAAMIAETIQUETAS";
   var version = "1.0";
   //Fin de Variables globales
@@ -23,80 +26,12 @@ $(document).ready(function() {
   //   console.log("Debug Logger Activado");
   // }
   if (endpoint == null) {
-    $('input#endpoint').val("http://breeze2-213.collaboratory.avaya.com/services/EventingConnector/events");
+    $('input#endpoint').val("https://breeze2-213.collaboratory.avaya.com/services/EventingConnector/events");
   } else {
     $('input#endpoint').val(endpoint);
     //$("select#debugmode").val(debugmode).change();
   }
   //Fin de Comprobación
-  //Listeners botones
-  //Listener Boton Ajustes
-  $("#ajustes-btn").click(function() {
-    $('#modal-ajustes').modal('show');
-  });
-  //Listener Limpiar Ajustes
-  $("#limpiar-btn").click(function() {
-    $('#modal-ajustes').modal('hide');
-    localStorage.clear();
-    $('#modal-informacion').modal({
-      backdrop: 'static',
-      keyboard: true,
-      show: true
-    });
-    $("#mensaje-modal").text("Se han limpiado tus Ajustes.\nEsta página se actualizará automáticamente ");
-    setTimeout(function() {
-      location.reload(true);
-    }, 3000);
-  });
-  //Listeners para formularios
-  ///Ajustes
-  $("#ajustes-frm").submit(function() {
-    event.preventDefault();
-    var datos = $("#ajustes-frm").serializeArray();
-    var formData = JSON.parse(JSON.stringify(jQuery('#ajustes-frm').serializeArray()))
-    console.log(datos);
-    var endpoint = datos["0"].value;
-    // var debugmode = datos["1"].value;
-    localData.set('endpoint_v', endpoint);
-    // localData.set('debug_v', debugmode);
-
-    $('#modal-ajustes').modal('hide');
-    $('#modal-informacion').modal({
-      backdrop: 'static',
-      keyboard: true,
-      show: true
-    });
-    $("#mensaje-modal").text("Tus ajustes se han guardado. Recargando");
-    setTimeout(function() {
-      location.reload(true);
-    }, 5000);
-  });
-  ///Precio
-  $("#precio-frm").submit(function() {
-    btype = "AAAMIAETIQUETASPRECIO"
-    event.preventDefault();
-    var preciofrm = $("#precio-frm").serializeArray();
-    console.log(preciofrm);
-    var emailval = preciofrm["0"].value;
-    var smsval = preciofrm["1"].value;
-    var prodval = preciofrm["2"].value;
-    var obj = "{\"email\":\"" + emailval + "\",\"sms\":\"" + smsval + "\",\"codigoProducto\":\"" + prodval + "\"}";
-
-
-    postbreeze(family, btype, version, endpoint, obj)
-  });
-  ///Precio
-  $("#descr-frm").submit(function() {
-    btype1 = "AAAMIAETIQUETASDESCRIPCION";
-    event.preventDefault();
-    var descrfrm = $("#descr-frm").serializeArray();
-    var phoneval1 = descrfrm["0"].value;
-    var prodval1 = descrfrm["1"].value;
-    var obj1 = "{\"mobileNumber\":\"" + phoneval1 + "\",\"productcode\":\"" + prodval1 + "\"}";
-    console.log(descrfrm);
-    postbreeze(family, btype1, version, endpoint, obj1)
-  });
-
   //Validacion E164
   var input = document.querySelector("#inputsms-txt"),
     errorMsg = document.querySelector("#error-msg"),
@@ -110,6 +45,8 @@ $(document).ready(function() {
 
   // initialise plugin
   var iti = window.intlTelInput(input, {
+    preferredCountries: ["us", "mx"],
+    hiddenInput: "full_phone",
     initialCountry: "auto",
     geoIpLookup: function(callback) {
       $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
@@ -122,6 +59,7 @@ $(document).ready(function() {
   });
 
   var iti2 = window.intlTelInput(input2, {
+    hiddenInput: "full_phone2",
     initialCountry: "auto",
     geoIpLookup: function(callback) {
       $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
@@ -190,7 +128,81 @@ $(document).ready(function() {
   input.addEventListener('change', reset);
   input.addEventListener('keyup', reset);
 
+  input2.addEventListener('change', reset);
+  input2.addEventListener('keyup', reset);
+
   //FIN Validacion
+
+
+  //Listeners botones
+  //Listener Boton Ajustes
+  $("#ajustes-btn").click(function() {
+    $('#modal-ajustes').modal('show');
+  });
+  //Listener Limpiar Ajustes
+  $("#limpiar-btn").click(function() {
+    $('#modal-ajustes').modal('hide');
+    localStorage.clear();
+    $('#modal-informacion').modal({
+      backdrop: 'static',
+      keyboard: true,
+      show: true
+    });
+    $("#mensaje-modal").text("Se han limpiado tus Ajustes.\nEsta página se actualizará automáticamente ");
+    setTimeout(function() {
+      location.reload(true);
+    }, 3000);
+  });
+  //Listeners para formularios
+  ///Ajustes
+  $("#ajustes-frm").submit(function() {
+    event.preventDefault();
+    var datos = $("#ajustes-frm").serializeArray();
+    var formData = JSON.parse(JSON.stringify(jQuery('#ajustes-frm').serializeArray()))
+    console.log(datos);
+    var endpoint = datos["0"].value;
+    // var debugmode = datos["1"].value;
+    localData.set('endpoint_v', endpoint);
+    // localData.set('debug_v', debugmode);
+
+    $('#modal-ajustes').modal('hide');
+    $('#modal-informacion').modal({
+      backdrop: 'static',
+      keyboard: true,
+      show: true
+    });
+    $("#mensaje-modal").text("Tus ajustes se han guardado. Recargando");
+    setTimeout(function() {
+      location.reload(true);
+    }, 5000);
+  });
+  ///Precio
+  $("#precio-frm").submit(function() {
+    btype = "AAAMIAETIQUETASPRECIO"
+    event.preventDefault();
+    var preciofrm = $("#precio-frm").serializeArray();
+    console.log(preciofrm);
+    var emailval = preciofrm["0"].value;
+    var smsval = preciofrm["1"].value;
+    var prodval = preciofrm["2"].value;
+    var obj = "{\"email\":\"" + emailval + "\",\"sms\":\""+ smsval +"\",\"codigoProducto\":\"" + prodval + "\"}";
+    console.log(obj);
+
+    postbreeze(family, btype, version, endpoint, obj)
+  });
+  ///Precio
+  $("#descr-frm").submit(function() {
+    btype1 = "AAAMIAETIQUETASDESCRIPCION";
+    event.preventDefault();
+    var descrfrm = $("#descr-frm").serializeArray();
+    var phoneval1 = descrfrm["0"].value;
+    var prodval1 = descrfrm["1"].value;
+    var obj1 = "{\"mobileNumber\":\""+ phoneval1 +"\",\"productcode\":\"" + prodval1 + "\"}";
+    console.log(descrfrm);
+    postbreeze(family, btype1, version, endpoint, obj1)
+  });
+
+
 
 
   //Fin Listeners
